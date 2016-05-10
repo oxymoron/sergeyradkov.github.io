4fdfa2bfaeeab44ca5ab6b8e5e6aa8747d1d0ea9
sergeyradkov <reriic@ya.ru>
2016-05-09 23:51:03 -0600

    test ng

--- app/app.js
+++ app/app.js
@@ -1,14 +1,14 @@
 (function(){var app = angular.module('resume', ['ui.router'])})();
 
     
-angular.module('resume').config(function($stateProvider){
+/*angular.module('resume').config(function($stateProvider){
     
     $stateProvider
         .state('home',{
         url: '',
         template: '<about-me></about-me>'
         })
-});
+}); */
 
 angular.module('resume').component('aboutMe', {
         templateUrl: 'pages/about.html',
