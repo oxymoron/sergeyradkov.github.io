ebbcac7e84df5c2d7f6f66f84f8c9a0b3a553bc2
sergeyradkov <reriic@ya.ru>
2016-05-09 23:28:54 -0600

    qwe

--- app/app.js
+++ app/app.js
@@ -1,15 +1,17 @@
 (function(){var app = angular.module('resume', ['ui.router'])})();
 
+    
 angular.module('resume').config(function($stateProvider){
-    $stateProvider.state('main', {url:'', template: '<main-page></main-page>'}
-                  .state('about',{url:'/pages/about.html',template: '<about></about>'})
-    );
-
+    
+    $stateProvider
+        .state('home',{
+        url: '',
+        template: '<main-page></main-page>'
+        })
 });
 
 angular.module('resume').component('mainPage', {
         templateUrl: 'pages/main.html',
-        controllerAs: 'vm',
         controller: function(){}
         
-        });
+});
