3163a26303741fe91f6354e31066903dd5e78dd6
sergeyradkov <reriic@ya.ru>
2016-05-09 23:47:55 -0600

    new 6

--- app/app.js
+++ app/app.js
@@ -15,3 +15,23 @@ angular.module('resume').component('aboutMe', {
         controller: function(){}
         
 });
+angular.module('resume').component('abilities', {
+        templateUrl: 'pages/abilities.html',
+        controller: function(){}
+        
+});
+angular.module('resume').component('experience', {
+        templateUrl: 'pages/experience.html',
+        controller: function(){}
+        
+});
+angular.module('resume').component('portfolio', {
+        templateUrl: 'pages/portfolio.html',
+        controller: function(){}
+        
+});
+angular.module('resume').component('contacts', {
+        templateUrl: 'pages/contacts.html',
+        controller: function(){}
+        
+});
