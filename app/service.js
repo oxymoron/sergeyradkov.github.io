a0ac516e1b860af1244a2e1756f4bba5485fe311
sergeyradkov <radkov@gmail.com>
2016-05-18 11:04:36 -0600

    123

--- app/service.js
+++ app/service.js
@@ -4,19 +4,19 @@ angular.module('resume')
 	var imgRoot = 'asset/img/';
 
     socialLinks = [{
-            url: "https://www.facebook.com/radkov.sn",
+            url: 'https://www.facebook.com/radkov.sn',
 			title: "Facebook",
 			img: imgRoot + 'facebook_icon.png'
 		    }, {
-            url: "https://www.linkedin.com/in/radkovsn",
+            url: 'https://www.linkedin.com/in/radkovsn',
 			title: "LikedIn",
 			img: imgRoot + 'linkedin_icon.png'
 			}, {
-            url: "https://github.com/sergeyradkov",
+            url: 'https://github.com/sergeyradkov',
 			title: "GitHub",
 			img: imgRoot + 'github_icon.png'
 			}, {
-            url: "https://www.instagram.com/sergeriic/",
+            url: 'https://www.instagram.com/sergeriic/',
 			title: "Instagram",
 			img: imgRoot + 'instagram_info.png'
 	 }];
