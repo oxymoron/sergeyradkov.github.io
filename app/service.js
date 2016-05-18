7f0329adee1be2373c541159874d41073da3bd5e
sergeyradkov <radkov@gmail.com>
2016-05-18 14:29:41 -0600

    123

--- app/service.js
+++ app/service.js
@@ -25,12 +25,55 @@ angular.module('resume')
          return socialLinks;
      }
      tools = [{
-            url: 'https://www.facebook.com/radkov.sn',
-			title: "Facebook",
-			img: imgRoot + 'facebook_icon.png'
+            url: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
+			title: 'HTML',
+			img: imgRoot + 'html.png'
             },{
-         
+            url: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+			title: 'CSS',
+			img: imgRoot + 'css.png'
+            },{
+            url: 'http://getbootstrap.com/',
+			title: 'Bootstrap',
+			img: imgRoot + 'bootstrap.png'
+            },{
+            url: 'http://jquery.com/',
+			title: 'jQuery',
+			img: imgRoot + 'jquery.png'                                       
+            },{
+            url: 'https://angularjs.org/',
+			title: 'AngularJS',
+			img: imgRoot + 'angular.png' 
+            },{
+            url: 'https://www.firebase.com/',
+			title: 'Firebase',
+			img: imgRoot + 'firebase.png'                                       
+            },{
+            url: 'https://developer.mozilla.org/en/docs/Web/JavaScript',
+			title: 'JavaScript',
+			img: imgRoot + 'javascript.png'                                       
+            },{
+            url: '',
+			title: 'C#',
+			img: imgRoot + 'csharp.png'                                       
+            },{
+            url: 'https://github.com/',
+			title: 'GitHub',
+			img: imgRoot + 'github.png'                                       
+            },{
+            url: 'https://www.microsoft.com/net/default.aspx',
+			title: 'Microsoft.NET',
+			img: imgRoot + 'dotnet.png'                                       
+            },{
+            url: 'https://www.visualstudio.com/',
+			title: 'Visual Studio',
+			img: imgRoot + 'vs.png'                                       
+            },{
+            url: 'https://www.jetbrains.com/idea/',
+			title: 'Intellij IDEA',
+			img: imgRoot + 'idea.png'                                                                  
             }]
+            
      this.getTools = function(){
          
      }
@@ -38,66 +81,3 @@ angular.module('resume')
          
      }
 });
-
-                    <div class="icon-list">
-                        <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5" target="_blank">
-                        <img title="HTML" src="asset/img/html.png" alt="html">
-                        </a>
-                    </div>
-                    <div  class="icon-list">
-                        <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3" target="_blank">
-                        <img title="CCS3" src="asset/img/css.png" alt="css">
-                        </a>
-                    </div>
-                    <div class="icon-list">
-                        <a target="_blank" href="http://getbootstrap.com/">
-                        <img title="Bootstarp" src="asset/img/bootstrap.png" alt="bootstrap" target="_blank">
-                        </a>
-                     </div>
-                     <div class="icon-list">
-                        <a target="_blank" href="http://jquery.com/">
-                        <img title="jQuery" src="asset/img/jquery.png" alt="jquery" target="_blank">
-                        </a>
-                     </div>
-                     <div class="icon-list">
-                        <a target="_blank" href="https://angularjs.org/">
-                        <img title="AngularJS" src="asset/img/angular.png" alt="angular" target="_blank">
-                        </a>
-                     </div>
-                     <div class="icon-list">
-                        <a target="_blank" href="https://www.firebase.com/">
-                        <img title="Firebase" src="asset/img/firebase.png" alt="firebase" target="_blank">
-                        </a>
-                    </div>
-
-                    <div class="icon-list">
-                        <a target="_blank" href="https://developer.mozilla.org/en/docs/Web/JavaScript">
-                        <img title="JavaScript" src="asset/img/javascript.png" alt="javascript" target="_blank">
-                        </a>
-                    </div>               
-                    <div class="icon-list">
-                        <a target="_blank" href="https://github.com/">
-                        <img title="C#" src="asset/img/csharp.png" alt="c#" target="_blank">
-                        </a>
-                    </div>
-                    <div class="icon-list">
-                        <a target="_blank" href="https://github.com/">
-                        <img title="GitHub" src="asset/img/github.png" alt="github" target="_blank">
-                        </a>
-                    </div>
-                    <div class="icon-list">
-                        <a target="_blank" href="https://www.microsoft.com/net/default.aspx">
-                        <img title="Microsoft .NET" src="asset/img/dotnet.png" alt=".net" target="_blank">
-                        </a>
-                    </div>           
-                    <div class="icon-list">
-                         <a target="_blank" href="https://www.visualstudio.com/">
-                         <img title="Visual Studio" src="asset/img/vs.png" alt="visualstudio" target="_blank">
-                         </a>
-                    </div>
-
-                    <div class="icon-list">
-                            <a target="_blank" href="https://www.jetbrains.com/idea/">
-                            <img title="Intellij IDEA" src="asset/img/idea.png" alt="idea" target="_blank">
-                            </a>
-                    </div>
